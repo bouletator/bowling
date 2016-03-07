e536cad38498595d8e1285117885505ee2a8cb6b
21504999 <pitoux16@gmail.com>
2016-03-07 09:39:37 +0100

    #1 classe Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+/**
+ * Created by 21504999 on 07/03/2016.
+ */
+public class Main {
+
+    public static void main()
+    {
+        ArrayList<Frame> list = new ArrayList<Frame>(10);
+
+
+        for (int i = 0; i < list.size(); i++)
+        {
+
+        }
+    }
+}
