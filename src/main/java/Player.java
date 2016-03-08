4fc41f7e678a50dde8d9e71a77ae89029947f8c0
bouletator <clement.romanet@gmail.com>
2016-03-08 10:30:37 +0100

    #2 Correction d'erreurs

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -20,21 +20,27 @@ public class Player {
         for(Frame f : listFrames){
             if(f.isStrike()){
                 nbFrameAfterStrike = 0;
-                strike = false;
+                strike = true;
             }
             else if (f.isSpare()){
                 spare = true;
             }
-            else{
-                if(spare){
+            else {
+                if (spare) {
                     sum += 10 + f.getScore_lancer_1();
                     spare = false;
                 }
-                if(strike && nbFrameAfterStrike == 2){
+                if (strike && nbFrameAfterStrike == 2) {
                     sum += 10 + totalStrike + f.getScore_lancer_1();
-                }
-                else if(strike && nbFrameAfterStrike == 1){
-                    totalStrike = f.calculScore();
+                    strike = false;
+                    totalStrike = 0;
+                    nbFrameAfterStrike = 0;
+                } else if (strike && nbFrameAfterStrike == 1) {
+                    totalStrike = f.getScore_lancer_1();
+                    nbFrameAfterStrike += 1;
+                } else if (strike){
+                    totalStrike = f.getScore_lancer_1();
+                    nbFrameAfterStrike += 1;
                 }
                 sum += f.calculScore();
             }
