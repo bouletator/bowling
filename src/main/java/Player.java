9d50a66e7e1abf698425748f994775b039586c42
bouletator <clement.romanet@gmail.com>
2016-03-08 10:25:22 +0100

    #2 Realisation de la fonction de calcul du score

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -13,13 +13,30 @@ public class Player {
 
     public int getScore(){
         int sum = 0;
-        int strike = 0;
+        boolean strike = false;
         int nbFrameAfterStrike = 0;
-        int spare = 0;
-        int nbFrameAfterSpare = 0;
+        boolean spare = false;
+        int totalStrike = 0;
         for(Frame f : listFrames){
             if(f.isStrike()){
                 nbFrameAfterStrike = 0;
+                strike = false;
+            }
+            else if (f.isSpare()){
+                spare = true;
+            }
+            else{
+                if(spare){
+                    sum += 10 + f.getScore_lancer_1();
+                    spare = false;
+                }
+                if(strike && nbFrameAfterStrike == 2){
+                    sum += 10 + totalStrike + f.getScore_lancer_1();
+                }
+                else if(strike && nbFrameAfterStrike == 1){
+                    totalStrike = f.calculScore();
+                }
+                sum += f.calculScore();
             }
         }
         return sum;
